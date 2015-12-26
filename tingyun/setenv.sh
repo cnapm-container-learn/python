427fc1be3b2ddf75a2b19281a88820509f2f72fe
wdxxs2z <243219650@qq.com>
2015-12-26 23:20:49 +0800

    Update tingyun python agent.

---
+++ tingyun/setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export TING_YUN_CONFIG_FILE=${TINGYUN_HOME}/tingyun.ini
+
+tingyun-admin generate-config ${TINGYUN_LICENSE_KEY} ${TINGYUN_HOME}/tingyun.ini
+
+sed -i "s#^app_name = .*#app_name = ${TINGYUN_APP_NAME}#g"  ${TINGYUN_HOME}/tingyun.ini
