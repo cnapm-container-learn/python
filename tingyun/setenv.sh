adbb682ad6cb58d39b617c2db130de409bb95360
wdxxs2z <243219650@qq.com>
2015-12-27 23:51:08 +0800

    Fix example server.py not start bug.

--- tingyun/setenv.sh
+++ tingyun/setenv.sh
@@ -5,3 +5,5 @@ export TING_YUN_CONFIG_FILE=${TINGYUN_HOME}/tingyun.ini
 tingyun-admin generate-config ${TINGYUN_LICENSE_KEY} ${TINGYUN_HOME}/tingyun.ini
 
 sed -i "s#^app_name = .*#app_name = ${TINGYUN_APP_NAME}#g"  ${TINGYUN_HOME}/tingyun.ini
+
+tingyun-admin run-program nohup python ${TINGYUN_HOME}/server.py >logfile.txt 2>&1 </dev/null
