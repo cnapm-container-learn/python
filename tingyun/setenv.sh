9935ba2073e256ea03b1b3f376032003d132aaac
wdxxs2z <243219650@qq.com>
2015-12-27 14:15:28 +0800

    Add an tingyun python example.

--- tingyun/setenv.sh
+++ tingyun/setenv.sh
@@ -5,3 +5,5 @@ export TING_YUN_CONFIG_FILE=${TINGYUN_HOME}/tingyun.ini
 tingyun-admin generate-config ${TINGYUN_LICENSE_KEY} ${TINGYUN_HOME}/tingyun.ini
 
 sed -i "s#^app_name = .*#app_name = ${TINGYUN_APP_NAME}#g"  ${TINGYUN_HOME}/tingyun.ini
+
+tingyun-admin run-program python ${TINGYUN_HOME}/server.py
