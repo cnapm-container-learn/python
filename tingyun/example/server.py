83fad97d75037a3dd684ba0728173d8f159a06b7
wdxxs2z <243219650@qq.com>
2015-12-28 13:57:32 +0800

    Update python wsgi web example.

--- tingyun/example/server.py
+++ tingyun/example/server.py
@@ -1,19 +1,6 @@
-import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
- 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8000
-server_address = ('', port)
- 
-HandlerClass.protocol_version = Protocol
-httpd = ServerClass(server_address, HandlerClass)
- 
-sa = httpd.socket.getsockname()
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
+from wsgiref.simple_server import make_server
+from hello import application
+
+httpd = make_server('', 8000, application)
+print "Serving HTTP on port 8000..."
 httpd.serve_forever()
